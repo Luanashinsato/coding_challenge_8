bf6f479ec419437324e2f71c8f7b871aa27a718a
Luanashinsato <lshinsato@usf.edu>
2025-02-16 19:58:57 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -7,3 +7,14 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // Testing the function 
 calculateSalary(5000, 500, 0.1); 
 calculateSalary(7000, 1000, 0.15); 
+
+// Task 2: Function Expression
+// Declaring a function expression to determine the final price
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    console.log(`Sales Tax: $${finalPrice}`);
+}
+// Testing the data 
+calculateDiscount(100, 0.2); 
+calculateDiscount(250, 0.15); 
+
